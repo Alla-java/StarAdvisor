772c288d9385b2f97751697bdf74455d0d3afe6a
Alla-java <goneman@yandex.ru>
2025-05-23 12:28:22 +0300

    Первый коммит

---
+++ src/main/java/org/skypro/staradvisor/StarAdvisorApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.staradvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/staradvisor/StarAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.staradvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
