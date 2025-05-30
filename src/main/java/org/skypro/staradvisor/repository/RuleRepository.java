c20460bcdc318ea19a38cd9a866418b5f84af8a5
IgorKa45 <igorkanasnik@gmail.com>
2025-05-30 21:53:10 +0400

    -Добавил в конфигурацию DataSource метод для того чтобы подключить вторую базу данных

--- src/main/java/org/skypro/staradvisor/repository/RuleRepository.java
+++ src/main/java/org/skypro/staradvisor/repository/RuleRepository.java
@@ -2,7 +2,7 @@ package org.skypro.staradvisor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import model.RecommendationRule;
+import  org.skypro.staradvisor.model.RecommendationRule;
 
 
 import java.util.Optional;
